5dcec0a00e19bd00533667844d1c4ff748987f92
FarhaIslam <farhaislam999@gamil.com>
2017-11-29 20:14:48 -0800

    Calculator complete which is perform addition , subtraction , multiplication , division and the power. If  type some operator which is beyond of the function of the calculator then some message is printing.

--- Calculator/src/Calculator.java
+++ Calculator/src/Calculator.java
@@ -1,9 +1,56 @@
+import java.util.Scanner;
 
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+		String a;
+		while(true)
+		{
+			System.out.println(">");
+			a = sc.next();
+			double first = Double.parseDouble(a);
+			if(first == 0)
+				break;
+			a = sc.next();
+			if(a.charAt(0) == '+')
+			{
+					a = sc.next();
+					double second = Double.parseDouble(a);
+					System.out.println(second + first);
+			}
+			else if(a.charAt(0) == '-')
+			{
+				a = sc.next();
+				double second = Double.parseDouble(a);
+				System.out.println(first - second);
+			}
+			else if(a.charAt(0) == '*')
+			{
+				a = sc.next();
+				double second = Double.parseDouble(a);
+				System.out.println(first * second);
+			}
+			else if(a.charAt(0) == '/')
+			{
+				a = sc.next();
+				double second = Double.parseDouble(a);
+				System.out.println(first / second);
+			}
+			else if(a.charAt(0) == '^')
+			{
+				a = sc.next();
+				double second = Double.parseDouble(a);
+				System.out.println(Math.pow(first , second));
+			}
+			else {
+				a = sc.next();
+				System.out.println("Typing operator is beyond the scope of this calculator.");
+			}
+		}
+		
+		System.out.println("Bye, now!");
+	  
 	}
 
 }
